4d26ea2e083e5872fdc5f653b0ba5fe48d52813b
Maarten Bode <maarten@orangetalent.nl>
2022-06-08 11:33:18 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# laravel-nl-language-pack
-Dutch translation files for Laravel
+# NL Language Pack for Laravel
+
+Dutch [translation files](https://laravel.com/docs/9.x/localization) for Laravel.
+
+### OrangeTalent
+
+OrangeTalent is the digital agency that makes you excel. We translate great ideas into resounding digital results. We build high-quality web applications and e-commerce solutions with Laravel, React and AWS.
---
+++ nl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Deze inloggegevens kloppen niet.',
+    'password' => 'Je opgegeven wachtwoord klopt niet.',
+    'throttle' => 'Teveel inlogpogingen, probeer het opnieuw over :seconds seconden.',
+
+];
